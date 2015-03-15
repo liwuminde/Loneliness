c43037b940018b5ca9447b6c3e89cd57bc1bccf5
qianfeng <qianfeng@Nius.local>
2015-03-15 15:40:15 +0800

    Initial Commit

---
+++ Loneliness/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Loneliness
+//
+//  Created by qianfeng on 15/3/15.
+//  Copyright (c) 2015年 曾维俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Loneliness/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Loneliness
+//
+//  Created by qianfeng on 15/3/15.
+//  Copyright (c) 2015年 曾维俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
